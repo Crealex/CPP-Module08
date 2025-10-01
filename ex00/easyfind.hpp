1963abe40f6a421c2d5220c038abd00bcda71d11
Crealex <alexandre.tomasi80@gmail.com>
2025-10-01 14:11:08 +0200

    finish ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -2,12 +2,47 @@
 
 # define EASYFIND_HPP
 
+#include "colors.hpp"
 # include <list>
+# include <vector>
+# include <deque>
+# include <algorithm>
+# include <exception>
+# include <iostream>
+
+class unfoundException: public std::exception
+{
+	public:
+		virtual const char *what() const throw(){return ("Number unfound in the container");};
+};
 
 template<typename T>
-int	easyfind(T list, int tofind)
+typename T::iterator	easyfind(T &container, int tofind)
 {
-	return (0);
+	typename T::iterator num = std::find(container.begin(), container.end(), tofind);
+	if (num == container.end())
+		throw (::unfoundException());
+	return (num);
+}
+
+template<typename T>
+typename T::const_iterator	easyfind(T const &container, int tofind)
+{
+	typename T::const_iterator num = std::find(container.begin(), container.end(), tofind);
+	if (num == container.end())
+		throw (::unfoundException());
+	return (num);
+}
+
+template<typename T>
+void	printContainer(T cont)
+{
+	for (typename T::iterator it = cont.begin(); it != cont.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
 }
 
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,2 +1,125 @@
 
 #include "easyfind.hpp"
+
+int main(void)
+{
+
+	// ---------- LIST ----------
+	std::list<int> list;
+
+	list.push_back(5);
+	list.push_back(4);
+	list.push_back(42);
+	list.push_back(666);
+
+	std::cout << BOLD << "*** TRY WITH A LIST ***" << RESET << std::endl;
+	std::cout << "List: " << std::endl;
+	::printContainer(list);
+	try
+	{
+		std::cout << "try to find 5" << std::endl;
+		std::cout << GREEN << "Result: " << *easyfind(list, 5) << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
+	try
+	{
+		std::cout << "try to find 1" << std::endl;
+		std::cout << GREEN << "Result: " << *easyfind(list, 1) << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
+
+	// ---------- VECTOR ----------
+
+	std::vector<int> vect;
+
+	vect.push_back(9);
+	vect.push_back(42);
+	vect.push_back(6);
+	vect.push_back(9);
+	vect.push_back(667);
+	std::cout << BOLD << "*** TRY WITH A VECTOR ***" << RESET << std::endl;
+	std::cout << "vect: " << std::endl;
+	::printContainer(vect);
+	try
+	{
+		std::cout << "try to find 5" << std::endl;
+		std::cout << GREEN << "Result: " << *easyfind(vect, 5) << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
+	try
+	{
+		std::cout << "try to find 42" << std::endl;
+		std::cout << GREEN << "Result: " << *easyfind(vect, 42) << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
+
+	// ---------- DEQUE ----------
+
+	std::deque<int> deq;
+
+	deq.push_back(1);
+	deq.push_back(3);
+	deq.push_back(6);
+	deq.push_back(999);
+	deq.push_back(666);
+	std::cout << BOLD << "*** TRY WITH A DEQUE ***" << RESET << std::endl;
+	std::cout << "deq: " << std::endl;
+	::printContainer(deq);
+	try
+	{
+		std::cout << "try to find 3" << std::endl;
+		std::cout << GREEN << "Result: " << *easyfind(deq, 3) << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
+	try
+	{
+		std::cout << "try to find 42" << std::endl;
+		std::cout << GREEN << "Result: " << *easyfind(deq, 42) << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
+
+
+	// ---------- CONST VECTOR ----------
+
+	const std::vector<int> vect_c;
+
+	std::cout << BOLD << "*** TRY WITH A CONST VECTOR ***" << RESET << std::endl;
+	std::cout << "vect_c: " << " (impossible to push elements in const container)" << std::endl;
+	::printContainer(vect_c);
+	try
+	{
+		std::cout << "try to find 5" << std::endl;
+		std::cout << GREEN << "Result: " << *easyfind(vect_c, 5) << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
+	try
+	{
+		std::cout << "try to find 42" << std::endl;
+		std::cout << GREEN << "Result: " << *easyfind(vect_c, 42) << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
+}
